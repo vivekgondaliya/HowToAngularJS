2f56c54043682ba6669c77e35f18fa0caa17888e
Vivek Gondalia <vg13@zips.uakron.edu>
2016-09-23 19:23:26 -0400

    Code working, as was working on an incorrect JS file.

--- Controllers 1/js/app.js
+++ Controllers 1/js/app.js
@@ -1,16 +1,30 @@
+(function(){
+
 angular.module('invoice1',[])
-.controller('InvoiceController', function InvoiceController(){
+.controller('InvoiceController', InvoiceController);
+
+function InvoiceController(){
+	
 	this.qty = 1;
 	this.cost = 2;
-	this.inCurr = 'EUR';
-	this.currencies = ['USD', 'EUR', 'CNY'];
+	this.currencies = [{text:'USD'}, {text:'EUR'}, {text:'CNY'}];
+	this.inCurr = this.currencies[0];
 	this.usdToForeignRates = {
 		USD: 1,
 		EUR: 0.74,
 		CNY: 6.09
 	};
+	
+	this.total = function total(outCurr){
+		return this.convertCurrency(this.qty * this.cost, this.inCurr, outCurr);
+	};
+
+	this.convertCurrency = function convertCurrency(amount, inCurr, outCurr){
+		return amount * this.usdToForeignRates[outCurr.text]/this.usdToForeignRates[inCurr];
+	};
+
 	this.pay = function pay(){
 		window.alert('Thanks!');
 	};
-})
-;
+}
+}())
--- js/app.js
+++ js/app.js
@@ -1,16 +1,27 @@
 angular.module('invoice1',[])
-.controller('InvoiceController', function InvoiceController(){
+.controller('InvoiceController', function InvoiceController($scope){
 	this.qty = 1;
 	this.cost = 2;
-	this.inCurr = 'EUR';
-	this.currencies = ['USD', 'EUR', 'CNY'];
+	console.log("yo");
+	this.currencies = [{text:'USD'}, {text:'EUR'}, {text:'CNY'}];
+	this.inCurr = this.currencies[0];
 	this.usdToForeignRates = {
 		USD: 1,
 		EUR: 0.74,
 		CNY: 6.09
 	};
-	this.pay = function pay(){
-		window.alert('Thanks!');
+	
+	this.total = function total(outCurr){
+		console.log("I'm here");
+		return this.convertCurrency(this.qty * this.cost, this.inCurr, outCurr);
 	};
-})
-;
+
+	this.convertCurrency = function convertCurrency(amount, inCurr, outCurr){
+		return amount * this.usdToForeignRates[outCurr]/this.usdToForeignRates[inCurr];
+	};
+
+	this.pay = function pay(){
+		window.alert('Thanks2!');
+	};
+});
+console.log("meh")
