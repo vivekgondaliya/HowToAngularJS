b75fa581ca20efa8d3cfe218aff9bf829f99d452
Vivek Gondalia <vivekgondalia@Viveks-MacBook-Pro.local>
2016-09-26 23:10:00 -0400

    Completed Sericves Tutorial. StaStarted MEAN stack Tutorial. Created html and JS file. JS file has a single module with an array of objects consisting Posts and Upvotes, which are displayed on Page via ng-repeat with filter.

---
+++ Flapper News/app.js
@@ -0,0 +1,14 @@
+var app = angular.module('flapperNews', []);
+
+app.controller('MainCtrl', [
+'$scope',
+function($scope){
+  $scope.posts = [
+  	{title:'Post 1', upvotes: 5},
+  	{title:'Post 2', upvotes: 9},
+  	{title:'Post 3', upvotes: 2},
+  	{title:'Post 4', upvotes: 7},
+  	{title:'Post 5', upvotes: 3}
+  ];
+
+}]);
